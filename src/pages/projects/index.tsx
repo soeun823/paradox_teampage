ecbe01fc19b0dd88f8710e911fa69effa2b31884
soeun823 <bsoeun866@gmail.com>
2025-06-30 06:40:40 +0900

    feat :: 프로젝트 페이지 구현

--- src/pages/projects/index.tsx
+++ src/pages/projects/index.tsx
@@ -1,10 +1,12 @@
 import type {FC} from "react";
+import Windeath44 from '@assets/windeath44.svg'
+import * as _ from "./style.ts"
 
 const Projects:FC = () => {
     return (
-        <>
-            프젝
-        </>
+        <_.main>
+            <_.Img src={Windeath44} alt={"프로젝트"}/>
+        </_.main>
     )
 }
 export default Projects;
---
+++ src/pages/projects/style.ts
@@ -0,0 +1,13 @@
+import styled from "@emotion/styled";
+
+
+export const main = styled.div`
+    height: 100vh;
+    font-family: var(--font);
+    margin: 0 200px;
+    background-color: var(--black);
+`;
+
+export const Img = styled.img`
+    width: 100%;
+`;
