31a934b97a0956aa2df268fd14ee9b523fb29394
soeun823 <bsoeun866@gmail.com>
2025-06-30 15:04:12 +0900

    feat :: 캐릭터 데이터셋

---
+++ src/datas/character.ts
@@ -0,0 +1,15 @@
+import min_tetoAI from '@assets/character/pixel/Teto AI.svg';
+import max_tetoAI from '@assets/character/real/tetoAI.svg';
+import min_teto from '@assets/character/pixel/kasane teto.svg';
+import max_teto from '@assets/character/real/kasane teto.svg';
+import min_gumi from '@assets/character/pixel/Gumi.svg';
+import max_gumi from '@assets/character/real/Gumi.svg';
+import min_miku from '@assets/character/pixel/Gumi.svg';
+import max_miku from '@assets/character/real/Gumi.svg';
+
+export default[
+  { "id": 0, "name":"teto AI", "pixel":min_tetoAI, "real":max_tetoAI},
+  { "id": 1, "name":"kasane teto", "pixel":min_teto, "real":max_teto},
+  { "id": 2, "name":"gumi", "pixel":min_gumi, "real":max_gumi},
+  { "id": 3, "name":"miku", "pixel":min_miku, "real":max_miku}
+]
