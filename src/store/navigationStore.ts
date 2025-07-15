bcb43707a1b752c964a2981ec2054f4c78148c1c
soeun823 <bsoeun866@gmail.com>
2025-07-15 21:41:10 +0900

    feat :: 위치 변경 store 생성

---
+++ src/store/navigationStore.ts
@@ -0,0 +1,11 @@
+import { create } from 'zustand';
+
+interface LocationState {
+    isLocate: number;
+    setIsLocate: (value: number) => void;
+}
+
+export const useLocationStore = create<LocationState>((set) => ({
+    isLocate: 0,
+    setIsLocate: (value: number) => set({ isLocate: value }),
+}));
