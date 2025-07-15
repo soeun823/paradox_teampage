b9613f2a8ab5ca222604ad39abcb7ad59673f589
soeun823 <bsoeun866@gmail.com>
2025-07-15 21:42:47 +0900

    feat :: 위치 변경 시 스타일

--- src/components/button/style.ts
+++ src/components/button/style.ts
@@ -1,16 +1,16 @@
 import styled from "@emotion/styled";
 
-export const Button = styled.button`
+export const Button = styled.button<{isActive:boolean}>`
   padding: 8px 16px;
   font-size: 18px;
   background-color: var(--black);
   border: none;
-  color: ${(props) => props.color };
+  color: ${({isActive}) => !isActive ? 'var(--white)' : 'var(--primary)'};
   border-radius: 7.5px;
   cursor: pointer;
   outline: none;
   font-family: var(--font); 
   &:hover {
-    color: var(--primary);  
+    color: var(--light-primary);  
   }
 `;
