d126e387d617295b5934a89ca820dd8a45bd9a6c
soeun823 <bsoeun866@gmail.com>
2025-07-18 10:15:52 +0900

    docs :: 깃허브 추가

--- src/datas/profile.ts
+++ src/datas/profile.ts
@@ -5,9 +5,39 @@ import P4 from "@assets/p4.svg";
 import P5 from "@assets/p5.svg";
 
 export default [
-  { id: 0, name: "류승찬", role: "CEO / Frontend", img: P1 },
-  { id: 1, name: "방세준", role: "CTO / Backend", img: P2 },
-  { id: 2, name: "김유찬", role: "Frontend / Designer", img: P3 },
-  { id: 3, name: "박소은", role: "Frontend", img: P4 },
-  { id: 4, name: "박동현", role: "Backend", img: P5 },
+  {
+    id: 0,
+    name: "류승찬",
+    role: "CEO / Frontend",
+    img: P1,
+    github: "https://github.com/winshine0326",
+  },
+  {
+    id: 1,
+    name: "방세준",
+    role: "CTO / Backend",
+    img: P2,
+    github: "https://github.com/yuuki08noah",
+  },
+  {
+    id: 2,
+    name: "김유찬",
+    role: "Frontend / Designer",
+    img: P3,
+    github: "https://github.com/Roena0516",
+  },
+  {
+    id: 3,
+    name: "박소은",
+    role: "Frontend",
+    img: P4,
+    github: "https://github.com/soeun823",
+  },
+  {
+    id: 4,
+    name: "박동현",
+    role: "Backend",
+    img: P5,
+    github: "https://github.com/pdh0128",
+  },
 ];
