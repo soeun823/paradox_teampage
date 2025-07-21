45948927726ab6fcccf87048f182e29e0c6b8389
soeun823 <bsoeun866@gmail.com>
2025-07-21 13:34:42 +0900

    delete :: 미사용시 삭제

--- src/datas/guest.ts
+++
@@ -1,9 +0,0 @@
-import { loadUserData } from "@/api/user";
-
-const guestData = await loadUserData();
-export const guest = guestData.map((user) => ({
-  id: user.id,
-  name: user.name,
-  comment: user.comment,
-}));
-console.log(guest);
